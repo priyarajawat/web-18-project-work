b1c423181ca36d5e73adb22c7d1b6b1f8d1bebd8
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2022-05-06 13:05:55 +0530

    added cart

---
+++ cart.js
@@ -0,0 +1,12 @@
+
+var data = JSON.parse(localStorage.getItem("item"));
+display(data);
+
+var newdata = JSON.parse(localStorage.getItem("newusercart"))|| [];
+
+
+// function display(data){
+//     data.forEach(function (elem){
+        
+//     })
+// }
