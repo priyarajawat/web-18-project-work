6771724bc671a81b26bdb97886647be518c65001
gouseimmu <gouseimmugh@gmail.com>
2022-05-06 21:42:32 +0530

    added files

---
+++ footwear.js
@@ -0,0 +1,251 @@
+var foot =[{
+    image_url:"https://www.switchbacktravel.com/sites/default/files/articles%20/Sports%20sandals%20%28hiking%20over%20log%20on%20beach%20in%20Chaco%20sandals%20-%20m%29.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+},
+{
+image_url:"https://www.uboric.com/wp-content/uploads/2021/02/DSC_0775_result-400x265.jpg",
+name : "FINACHI FINACHI Ultra Soft Modal Brief Pack...% Combed Bio Wash Cotton",
+strprice:"₹"+1499,
+price:"₹"+799.00,
+},
+{
+    image_url:" https://www.uboric.com/wp-content/uploads/2021/02/DSC_0807_result-400x265.jpg",
+name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+strprice:"₹"+1499,
+price:"₹"+799,
+
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2019/11/11-11116-400x267.png",
+name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+strprice:"₹"+1499,
+price:"₹"+799,
+},
+{
+    image_url:" https://www.uboric.com/wp-content/uploads/2021/01/DSC_0580_result-400x265.jpg",
+name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+strprice:"₹"+1499,
+price:"₹"+799,
+},
+{
+    image_url:" https://www.uboric.com/wp-content/uploads/2019/11/8-11001-400x267.png",
+name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+strprice:"₹"+1499,
+price:"₹"+799,
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2019/12/7-12571-400x265.jpg",
+name : "FINACHI 100% Combed FINACHI Ultra Soft Modal Brief Pack... Wash Cotton",
+strprice:"₹"+1499,
+price:"₹"+799,
+},
+{
+    image_url:" https://www.uboric.com/wp-content/uploads/2021/01/DSC_0580_result-400x265.jpg",
+name : "FINACHI 100%  Bio FINACHI Ultra Soft Modal Brief Pack... Cotton",
+strprice:"₹"+1499,
+price:"₹"+799,
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2021/01/DSC_0580_result-400x265.jpg",
+name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+strprice:"₹"+1499.00,
+price:"₹"+799,
+},
+{
+    image_url:" https://www.uboric.com/wp-content/uploads/2021/02/DSC_0775_result-400x265.jpg",
+name : "FINACHI MEN SHIRT Magic cotton With digital...% Combed Bio Wash Cotton",
+strprice:"₹"+1199.00,
+price:"₹"+799.00,
+},
+{
+    image_url:"https://www.switchbacktravel.com/sites/default/files/articles%20/Sports%20sandals%20%28hiking%20over%20log%20on%20beach%20in%20Chaco%20sandals%20-%20m%29.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+
+},
+{
+    image_url:" https://www.uboric.com/wp-content/uploads/2021/01/DSC_0687_result-400x265.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+},
+{
+    image_url:" https://www.uboric.com/wp-content/uploads/2019/12/19-11048-400x267.png",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2021/02/DSC_0863_result-400x265.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2021/02/DSC_0775_result-400x265.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2021/01/DSC_0687_result-400x265.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2021/02/DSC_0863_result-400x265.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2021/01/DSC_0687_result-400x265.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+},
+{
+    image_url:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sandals-1622127128.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2021/01/DSC_0687_result-400x265.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+},
+{
+    image_url:"https://i.insider.com/61255de3de5f560019e891ee?width=700",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2021/02/DSC_0775_result-400x265.jpg",
+    name : "FINACHI FINACHI Ultra Soft Modal Brief Pack...% Combed Bio Wash Cotton",
+    strprice:"₹"+1499,
+    price:"₹"+799.00,
+    },
+    {
+        image_url:" https://www.uboric.com/wp-content/uploads/2021/02/DSC_0807_result-400x265.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499,
+    price:"₹"+799,
+    
+    },
+    {
+        image_url:"https://www.uboric.com/wp-content/uploads/2019/11/11-11116-400x267.png",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499,
+    price:"₹"+799,
+    },
+    {
+        image_url:" https://www.uboric.com/wp-content/uploads/2021/01/DSC_0580_result-400x265.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499,
+    price:"₹"+799,
+    },
+    {
+        image_url:" https://www.uboric.com/wp-content/uploads/2019/11/8-11001-400x267.png",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1499,
+    price:"₹"+799,
+    },
+    {
+        image_url:"https://www.uboric.com/wp-content/uploads/2019/12/7-12571-400x265.jpg",
+    name : "FINACHI 100% Combed FINACHI Ultra Soft Modal Brief Pack... Wash Cotton",
+    strprice:"₹"+1499,
+    price:"₹"+799,
+    },
+    {
+        image_url:"https://www.uboric.com/wp-content/uploads/2021/01/DSC_0687_result-400x265.jpg",
+        name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+        strprice:"₹"+1499.00,
+        price:"₹"+799.00,
+    },
+    {
+        image_url:"https://i.insider.com/61255de3de5f560019e891ee?width=700",
+        name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+        strprice:"₹"+1499.00,
+        price:"₹"+799.00,
+    },
+    {
+        image_url:"https://www.uboric.com/wp-content/uploads/2021/02/DSC_0775_result-400x265.jpg",
+        name : "FINACHI FINACHI Ultra Soft Modal Brief Pack...% Combed Bio Wash Cotton",
+        strprice:"₹"+1499,
+        price:"₹"+799.00,
+        },
+        {
+            image_url:" https://www.uboric.com/wp-content/uploads/2021/02/DSC_0807_result-400x265.jpg",
+        name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+        strprice:"₹"+1499,
+        price:"₹"+799,
+        
+        },
+        {
+            image_url:"https://www.uboric.com/wp-content/uploads/2019/11/11-11116-400x267.png",
+        name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+        strprice:"₹"+1499,
+        price:"₹"+799,
+        },
+];
+
+
+var arrfoot =JSON.parse(localStorage.getItem("cart")) || [];
+var foot_products = document.querySelector("#container")
+function display(data){
+    foot_products.innerHTML=""
+var dataoffoot =  foot.forEach(function(ele){
+    var box =   document.createElement("div");
+    var image = document.createElement("img");
+    image.src=ele.image_url;
+    image.addEventListener("click",function(){
+        footware(ele)
+    });
+    var name = document.createElement("h3");
+    name.innerText=ele.name;
+    var strikeprice = document.createElement("p");
+    strikeprice.innerText=ele.strprice;
+    var price = document.createElement("p")
+    price.innerText=ele.price;
+    box.append(image,name,strikeprice,price);
+    document.querySelector("#container").append(box);
+  
+  })
+}
+display(foot)
+
+  
+  document.querySelector("#category").addEventListener("click",submit)
+  function submit(){
+      var selected = document.getElementById("category").value;
+      console.log(selected)
+      if(selected=="womens clothing")
+      {
+          window.location.href="womens.html"
+      }
+      else if(selected=="Mens clothing")
+      {
+          window.location.href="products.html"
+      }
+      else if(selected=="footwares")
+      {
+          window.location.href="footwares.html"
+      }
+      
+  }
+
+    
+function footware(ele){
+    console.log(ele)
+   arrfoot.push(ele);
+   localStorage.setItem("cart",JSON.stringify(arrfoot));
+   
+   
+ }
---
+++ pro.js
@@ -0,0 +1,286 @@
+var mensdata =[
+    {
+        image_url:"https://www.uboric.com/wp-content/uploads/2021/11/Cool-Beard-Styles-You-Should-Try.jpg",
+        name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+        strprice:"₹"+1099.00,
+        price:"₹"+899.00,
+        id:17
+    
+    },
+    {
+        image_url:"https://www.uboric.com/wp-content/uploads/2022/02/51mRO9X8L._UL1000_-400x400.jpg",
+        name : "MENS SHIRT MAGIC COTTON",
+        strprice:"₹"+1459.00,
+        price:"₹"+799.00,
+        id:16
+    },
+    {
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/51s-EmuOnnL._UL1000_-1-400x400.jpg",
+    name : "BRANDED PACK OF SHIRTS",
+    strprice:"₹"+1599.00,
+    price:"₹"+999.00,
+    id:107
+},
+{
+image_url: "https://kxadmin.siyaram.com/product/5JHSHFR000JSP3500-37/660/JSP3500F-02_1.jpg.webp",
+name : "BRANDED PACK OF SHIRTS",
+strprice:"₹"+1499,
+price:"₹"+799.00,
+id:106
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/81OXF5PEo8L._UL1500_-400x400.jpg",
+name : "BRANDED PACK OF SHIRTS",
+strprice:"₹"+1399,
+price:"₹"+799,
+id:105
+
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/61-Qz6jP4QL._UL1000_-400x400.jpg",
+name : "BRANDED PACK OF SHIRTS",
+strprice:"₹"+1399,
+price:"₹"+799,
+id:104
+},
+{
+    image_url:" https://www.uboric.com/wp-content/uploads/2022/02/81BoaL5NdHL._UL1500_-400x400.jpg",
+name : "BRANDED PACK OF SHIRTS",
+strprice:"₹"+1399,
+price:"₹"+799,
+id:104
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2021/11/Cool-Beard-Styles-You-Should-Try.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1099.00,
+    price:"₹"+899.00,
+    id:17
+
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/51mRO9X8L._UL1000_-400x400.jpg",
+    name : "MENS SHIRT MAGIC COTTON",
+    strprice:"₹"+1459.00,
+    price:"₹"+799.00,
+    id:16
+},
+{
+    image_url:" https://www.uboric.com/wp-content/uploads/2022/03/61mqFWGAmQL._UL1500_-400x399.jpg",
+name : "MENS SHIRT MAGIC COTTON",
+strprice:"₹"+1299,
+price:"₹"+559,
+id:103
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/617adqEx3rL._UL1500_-400x400.jpg",
+name : "BRANDED PACK OF SHIRTS",
+strprice:"₹"+1699,
+price:"₹"+799,
+id:102
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/518vKyF4xL._UL1000_-400x400.jpg",
+name : "FINACHI 100% Combed FINACHI Ultra Soft Modal Brief Pack... Wash Cotton",
+strprice:"₹"+1799,
+price:"₹"+599,
+id:101
+},
+{
+    image_url:" https://www.uboric.com/wp-content/uploads/2022/02/514CuQ8DsaL._UL1000_-400x400.jpg",
+name : "MENS SHIRT MAGIC COTTON",
+strprice:"₹"+1899,
+price:"₹"+699,
+id:100
+},
+{
+    image_url:" https://www.uboric.com/wp-content/uploads/2022/02/51Zhp6D5w9L._UL1000_-400x400.jpg",
+name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+strprice:"₹"+1999.00,
+price:"₹"+1199,
+id:19
+},
+{
+    image_url:"https://assets.ajio.com/medias/sys_master/root/20210420/R70k/607ecc5baeb269a9e3972063/dennislingo_premium_attire_green_full_sleeves_slim_fit_shirt.jpg",
+name : "FINACHI MEN SHIRT Magic cotton With digital...% Combed Bio Wash Cotton",
+strprice:"₹"+1099.00,
+price:"₹"+899.00,
+id:18
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2021/11/Cool-Beard-Styles-You-Should-Try.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1099.00,
+    price:"₹"+899.00,
+    id:17
+
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/51mRO9X8L._UL1000_-400x400.jpg",
+    name : "MENS SHIRT MAGIC COTTON",
+    strprice:"₹"+1459.00,
+    price:"₹"+799.00,
+    id:16
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/61nB1ZuhBCL._UL1500_-400x400.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1469.00,
+    price:"₹"+999.00,
+    id:15
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/519oLttxcZL._UL1000_-3-400x400.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1939.00,
+    price:"₹"+599.00,
+    id:14
+},
+{
+    image_url:"https://assets.ajio.com/medias/sys_master/root/20210420/R70k/607ecc5baeb269a9e3972063/dennislingo_premium_attire_green_full_sleeves_slim_fit_shirt.jpg",
+    name : "BRANDED PACK OF SHIRTS",
+    strprice:"₹"+1299.00,
+    price:"₹"+699.00,
+    id:13
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/61wHli5TQuL._UL1500_-400x400.jpg",
+    name : "MENS SHIRT MAGIC COTTON",
+    strprice:"₹"+1499.00,
+    price:"₹"+799.00,
+    id:12
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/51ckNZsLWIL._UL1000_-400x400.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1497.00,
+    price:"₹"+999.00,
+    id:11
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/51ckNZsLWIL._UL1000_-400x400.jpg",
+    name : "MENS SHIRT MAGIC COTTON",
+    strprice:"₹"+1488.00,
+    price:"₹"+979.00,
+    id:10
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2021/11/Cool-Beard-Styles-You-Should-Try.jpg",
+    name : "FINACHI 100% FINACHI Ultra Soft Modal Brief Pack... Bio Wash Cotton",
+    strprice:"₹"+1099.00,
+    price:"₹"+899.00,
+    id:17
+
+},
+{
+    image_url:"https://www.uboric.com/wp-content/uploads/2022/02/51mRO9X8L._UL1000_-400x400.jpg",
+    name : "MENS SHIRT MAGIC COTTON",
+    strprice:"₹"+1459.00,
+    price:"₹"+799.00,
+    id:16
+},
+{
+    image_url: "https://www.uboric.com/wp-content/uploads/2022/02/51s-EmuOnnL._UL1000_-1-400x400.jpg",
+    name : "BRANDED PACK OF SHIRTS",
+    strprice:"₹"+1499,
+    price:"₹"+799.00,
+    id:106
+    },
+    {
+        image_url:"https://getketchadmin.getketch.com/product/8905404323248/660/KHSH000336_1.jpg",
+    name : "BRANDED PACK OF SHIRTS",
+    strprice:"₹"+1399,
+    price:"₹"+799,
+    id:105
+    
+    },
+    {
+        image_url: "https://www.uboric.com/wp-content/uploads/2022/02/51s-EmuOnnL._UL1000_-1-400x400.jpg",
+        name : "BRANDED PACK OF SHIRTS",
+        strprice:"₹"+1499,
+        price:"₹"+799.00,
+        id:106
+        },
+        {
+            image_url:"https://kxadmin.siyaram.com/product/5JHSHFR000JSP3500-37/660/JSP3500F-02_1.jpg.webp",
+        name : "BRANDED PACK OF SHIRTS",
+        strprice:"₹"+1399,
+        price:"₹"+799,
+        id:105
+        
+        },
+];
+
+
+var arr =JSON.parse(localStorage.getItem("cart")) || [];
+
+var mens_products = document.querySelector("#container")
+function display(data){
+    mens_products.innerHTML=""
+var data =  mensdata.forEach(function(ele){
+    
+  var box =   document.createElement("div");
+  var image = document.createElement("img");
+  image.src=ele.image_url;
+  image.addEventListener("click",function(){
+    mens(ele)
+});
+  var name = document.createElement("h3");
+  name.innerText=ele.name;
+  var strikeprice = document.createElement("p");
+  strikeprice.innerText=ele.strprice;
+  var price = document.createElement("p")
+  price.innerText=ele.price;
+  box.append(image,name,strikeprice,price);
+  document.querySelector("#container").append(box);
+  
+ 
+})
+}
+display(mensdata)
+  
+
+
+ 
+document.querySelector("#category").addEventListener("click",submit)
+function submit(){
+    var selected = document.getElementById("category").value;
+    console.log(selected)
+    if(selected=="womens clothing")
+    {
+        window.location.href="womens.html"
+    }
+    else if(selected=="Mens clothing")
+    {
+        window.location.href="products.html"
+    }
+    else if(selected=="footwares")
+    {
+        window.location.href="footwares.html"
+    }
+
+}
+
+
+function mens(ele){
+    console.log(ele)
+   arr.push(ele);
+   localStorage.setItem("cart",JSON.stringify(arr));
+   
+   
+ }
+
+ 
+
+
+    
+    
+    
+    
+    
+
+
+
+    
+
+    
