0b9ccd079c147f61d2cf4b0dd8cbda68f225a197
gouseimmu <gouseimmugh@gmail.com>
2022-05-07 13:55:12 +0530

    added

--- productpage2.js
+++ productpage2.js
@@ -2,162 +2,163 @@ var womenscloth = [
 {
     image_url:"https://www.uboric.com/wp-content/uploads/2022/03/071.jpg",
     name : "JACKET DRESS SET",
-    strprice:"₹"+1499.00,
-    price:"₹"+999.00,
+    strprice:1499.00,
+    price:999.00,
 },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/03/320.jpg",
         name : "BHANDEJ DHOTI SET ",
-        strprice:"₹"+1599.00,
-        price:"₹"+899.00,
+        strprice:1599.00,
+        price:899.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-16-at-5.52.20-PM-3-400x575.jpeg",
         name : "ACQUAMINE SET",
-        strprice:"₹"+1699.00,
-        price:"₹"+899.00,
+        strprice:1699.00,
+        price:899.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/01/UBO-400x533.jpg",
         name :  "TUNESET OF SAREE",
-        strprice:"₹"+1699.00,
-        price:"₹"+999.00,
+        strprice:1699.00,
+        price:999.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-20-at-6.33.05-PM-1-400x500.jpeg",
         name : "JACKET DRESS SET",
-        strprice:"₹"+1499.00,
-        price:"₹"+699.00,
+        strprice:1499.00,
+        price:699.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-25-at-12.48.22-PM-400x499.jpeg",
         name : "TUNICS",
-        strprice:"₹"+2499.00,
-        price:"₹"+999.00,
+        strprice:2499.00,
+        price:999.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-20-at-6.33.19-PM-1-400x500.jpeg",
         name : "black crown sharara set",
-        strprice:"₹"+2499.00,
-        price:"₹"+799.00,
+        strprice:2499.00,
+        price:799.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-20-at-6.51.18-PM-2-400x500.jpeg",
         name : "TUNICS",
-        strprice:"₹"+1499.00,
-        price:"₹"+799.00,
+        strprice:1499.00,
+        price:799.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/03/071.jpg",
         name : "JACKET DRESS SET",
-        strprice:"₹"+1999.00,
-        price:"₹"+799.00,
+        strprice:1999.00,
+        price:799.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-16-at-10.48.22-PM-400x471.jpeg",
         name : "TUNESET OF SAREE",
-        strprice:"₹"+1399.00,
-        price:"₹"+799.00,
+        strprice:1399.00,
+        price:799.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-15-at-11.59.33-AM-400x484.jpeg",
         name : "JACKET DRESS SET",
-        strprice:"₹"+2599.00,
-        price:"₹"+799.00,
+        strprice:2599.00,
+        price:799.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-16-at-5.52.20-PM-3-400x575.jpeg",
         name : "TUNICS",
-        strprice:"₹"+1899.00,
-        price:"₹"+799.00,
+        strprice:1899.00,
+        price:799.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-15-at-11.58.07-AM-1-400x499.jpeg",
         name : "black crown sharara set",
-        strprice:"₹"+1499.00,
-        price:"₹"+699.00,
+        strprice:1499.00,
+        price:699.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-16-at-10.48.24-PM-400x465.jpeg",
         name : "UNESET OF SAREE",
-        strprice:"₹"+2499.00,
-        price:"₹"+799.00,
+        strprice:2499.00,
+        price:799.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-15-at-12.00.15-PM-400x500.jpeg",
         name : "black crown sharara set",
-        strprice:"₹"+3499.00,
-        price:"₹"+899.00,
+        strprice:3499.00,
+        price:899.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-16-at-10.48.22-PM-2-400x500.jpeg",
         name : "black crown sharara set",
-        strprice:"₹"+1499.00,
-        price:"₹"+799.00,
+        strprice:1499.00,
+        price:799.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-15-at-12.04.52-PM-400x493.jpeg",
         name : "black crown sharara set",
-        strprice:"₹"+1999.00,
-        price:"₹"+999.00,
+        strprice:1999.00,
+        price:999.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-15-at-5.58.18-PM-1.jpeg",
         name : "TUNICS",
-        strprice:"₹"+1499.00,
-        price:"₹"+799.00,
+        strprice:1499.00,
+        price:799.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-15-at-11.58.23-AM-1-400x500.jpeg",
         name : "JACKET DRESS SET",
-        strprice:"₹"+1499.00,
-        price:"₹"+799.00,
+        strprice:1499.00,
+        price:799.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-15-at-12.01.22-PM-1-400x500.jpeg",
         name : "UNESET OF SAREE",
-        strprice:"₹"+1699.00,
-        price:"₹"+899.00,
+        strprice:1699.00,
+        price:899.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-15-at-11.58.54-AM.jpeg",
         name : "BHANDEJ DHOTI SET",
-        strprice:"₹"+1499.00,
-        price:"₹"+799.00,
+        strprice:1499.00,
+        price:799.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-16-at-5.52.55-PM-2-400x562.jpeg",
         name : "JACKET DRESS SET",
-        strprice:"₹"+1499.00,
-        price:"₹"+799.00,
+        strprice:1499.00,
+        price:799.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-15-at-12.00.35-PM-400x533.jpeg",
         name : "UNESET OF SAREE",
-        strprice:"₹"+1499.00,
-        price:"₹"+899.00,
+        strprice:1499.00,
+        price:899.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/03/071.jpg",
         name : "JACKET DRESS SET",
-        strprice:"₹"+1499.00,
-        price:"₹"+999.00,
+        strprice:1499.00,
+        price:999.00,
     },
     {
         image_url:"https://www.uboric.com/wp-content/uploads/2022/04/WhatsApp-Image-2022-04-15-at-12.04.52-PM-400x493.jpeg",
         name : "black crown sharara set",
-        strprice:"₹"+1999.00,
-        price:"₹"+999.00,
+        strprice:1999.00,
+        price:999.00,
     },
 ];
 
 
 
-var arrofwomen =JSON.parse(localStorage.getItem("cart")) || [];
+var arrofwomen =JSON.parse(localStorage.getItem("cart1")) || [];
+console.log(arrofwomen)
 var womens_products = document.querySelector("#container")
 function display(data){
     womens_products.innerHTML=""
-var dataofwomen =  womenscloth.forEach(function(ele){
+data.forEach(function(ele){
     var box =   document.createElement("div");
     var image = document.createElement("img");
     image.src=ele.image_url;
@@ -201,7 +202,7 @@ display(womenscloth)
 function womens(ele){
     console.log(ele)
    arrofwomen.push(ele);
-   localStorage.setItem("cart",JSON.stringify(arrofwomen));
+   localStorage.setItem("cart1",JSON.stringify(arrofwomen));
    
    
  }
